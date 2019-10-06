45a7df471634024e36c7bfefd357cdcd61bab5a5
mrlhz <mr__lhz@163.com>
2019-10-06 22:51:02 +0800

    feat: add movie filter handle method

--- src/api/movie.js
+++ src/api/movie.js
@@ -1,12 +1,33 @@
-const { subjects, total } = require('D:/books/mdn/data/movie.json')
+const { subjects, total } = require('D:/web/myblog/puppeteer/data/movie/top250.json')
 
-const tags = ['剧情', '喜剧', '动作', '爱情', '科幻', '动画', '悬疑', '惊悚', '恐怖', '犯罪', '同性', '音乐', '歌舞', '传记', '历史', '战争', '西部', '奇幻', '冒险', '灾难', '武侠', '情色']
+let tags = ['剧情', '喜剧', '动作', '爱情', '科幻', '动画', '悬疑', '惊悚', '恐怖', '犯罪', '同性', '音乐', '歌舞', '传记', '历史', '战争', '西部', '奇幻', '冒险', '灾难', '武侠', '情色']
+
+tags = tags.map((tag, index) => {
+  return { key: 'tag' + index, value: tag }
+})
 
 export function getMovie(params = {}) {
-  const { page = 1, limit = 20 } = params
+  const { page = 1, limit = 20, title = '', tag = '' } = params
+  let movies = []
+  let count = 0
+
+  if (title) {
+    movies = subjects.filter((item) => {
+      return item.title.indexOf(title) !== -1
+    })
+    count = movies.length
+  } else if (!title && tag) {
+    movies = subjects.filter((item) => {
+      return item.genres.indexOf(tag) !== -1
+    })
+    count = movies.length
+  } else {
+    movies = subjects
+    count = total
+  }
   return {
     tags,
-    total,
-    movies: subjects.slice((page - 1) * limit, page * limit)
+    total: count,
+    movies: movies.slice((page - 1) * limit, page * limit)
   }
 }
--- src/views/douban/movies/index.vue
+++ src/views/douban/movies/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="app-container">
+    <!-- filter -->
+    <div class="filter-container">
+      <el-input class="filter-item" style="width:200px" placeholder="名称" v-model="queryParameter.title" @keyup.enter.native="handleFilter" />
+      <el-select class="filter-item" v-model="queryParameter.tag" clearable placeholder="类型">
+        <el-option v-for="tag in tagTypeOptions" :key="tag.key" :label="tag.value" :value="tag.value" />
+      </el-select>
+      <el-button class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+        搜索
+      </el-button>
+    </div>
+    <!-- filter end -->
     <el-table
       v-loading="listLoading"
       :data="list"
@@ -8,29 +19,39 @@
       fit
       highlight-current-row
     >
-      <el-table-column label="ID" width="100" align="center">
+      <el-table-column label="ID" width="100px" align="center">
         <template slot-scope="scope">
           {{ scope.row.id }}
         </template>
       </el-table-column>
       <el-table-column prop="title" label="名称" width="400" />
+      <el-table-column prop="rating" label="评分" width="60" align="center" />
       <el-table-column prop="year" label="年份" width="60" align="center" />
-      <el-table-column label="类型">
+      <el-table-column label="类型" width="400">
         <template slot-scope="{row}">
           <div class="tag-group">
             <el-tag v-for="tag in row.genres" :key="tag" size="medium">{{ tag }}</el-tag>
           </div>
         </template>
       </el-table-column>
-      <el-table-column prop="rating.average" label="评分" width="60" align="center" />
+      <el-table-column prop="rating.average" label="操作" align="center">
+        <template slot-scope="{row}">
+          <el-button type="primary" size="mini" @click="handleDetails(row)">详情</el-button>
+        </template>
+      </el-table-column>
     </el-table>
+
     <pagination
       v-show="total > 0"
       :total="total"
-      :page.sync="page"
-      :limit.sync="limit"
+      :page.sync="queryParameter.page"
+      :limit.sync="queryParameter.limit"
       @pagination="fetchData"
     />
+
+    <el-dialog title="电影详情" :visible.sync="dialogTableVisible">
+      <div>{{ dialogTable.title }}</div>
+    </el-dialog>
   </div>
 </template>
 
@@ -44,25 +65,44 @@ export default {
   data() {
     return {
       listLoading: true,
-      page: 1,
-      limit: 20,
-      total: 0
+      list: [],
+      total: 0,
+      dialogTableVisible: false,
+      dialogTable: {},
+      queryParameter: {
+        page: 1,
+        limit: 20,
+        title: '',
+        tag: ''
+      },
+      tagTypeOptions: []
     }
   },
   computed: {},
   created() {
-    this.fetchData({
-      page: this.page,
-      limit: this.limit
-    })
+    this.fetchData()
   },
   methods: {
-    fetchData(params) {
+    fetchData() {
       this.listLoading = true
-      const { movies, total } = getMovie(params)
+      const { movies, total, tags } = getMovie(this.queryParameter)
       this.list = movies
       this.total = total
+      this.tagTypeOptions = tags
       this.listLoading = false
+    },
+    handleDetails(row) {
+      this.dialogTableVisible = true
+      console.log(Object.keys(row))
+      this.dialogTable = {
+        title: row.title
+      }
+    },
+    handleFilter() {
+      const params = this.queryParameter
+      const { page, title, tag } = params
+      console.log(page, title, tag)
+      this.fetchData()
     }
   }
 }
@@ -71,5 +111,14 @@ export default {
 <style lang="scss">
   .tag-group span{
     margin-left: 5px;
+  }
+
+  .filter-container {
+    padding-bottom: 10px;
+    .filter-item {
+      display: inline-block;
+      vertical-align: middle;
+      margin-bottom: 10px;
+    }
   }
 </style>
